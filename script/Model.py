32b610f83463e8ce28b0028a10856de20e735d56
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-03-07 16:36:56 +0700

    add Model and training code

---
+++ script/Model.py
@@ -0,0 +1,136 @@
+"""
+Script for regressor model generator
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.models import resnet18
+
+def OrientationLoss(orient_batch, orientGT_batch, confGT_batch):
+    """
+    Orientation loss function
+    """
+    batch_size = orient_batch.size()[0]
+    indexes = torch.max(confGT_batch, dim=1)[1]
+
+    # extract important bin
+    orientGT_batch = orientGT_batch[torch.arange(batch_size), indexes]
+    orient_batch = orient_batch[torch.arange(batch_size), indexes]
+
+    theta_diff = torch.atan2(orientGT_batch[:,1], orientGT_batch[:,0])
+    estimated_theta_diff = torch.atan2(orient_batch[:,1], orient_batch[:,0])
+
+    return -1 * torch.cos(theta_diff - estimated_theta_diff).mean()
+
+class ResNet18(nn.Module):
+    def __init__(self, model=None, bins=2, w=0.4):
+        super(ResNet18, self).__init__()
+        self.bins = bins
+        self.w = w
+        # extract all layer until [-2]
+        self.model = nn.Sequential(*(list(model.children())[:-2]))
+
+        # orientation head, for orientation estimation
+        self.orientation = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, bins*2) # 4 bins
+        )
+
+        # confident head, for orientation estimation
+        self.confidence = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, bins) # 2 bins   
+        )
+
+        # dimension head
+        self.dimension = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 3) # x, y, z
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 512)
+
+        orientation = self.orientation(x)
+        orientation = orientation.view(-1, self.bins, 2)
+        orientation = F.normalize(orientation, dim=2)
+        
+        confidence = self.confidence(x)
+
+        dimension = self.dimension(x)
+
+        return orientation, confidence, dimension
+
+class VGG11(nn.Module):
+    def __init__(self, model=None, bins=2, w=0.4):
+        super(ResNet18, self).__init__()
+        self.bins = bins
+        self.w = w
+        # extract all layer until [-2]
+        self.model = nn.Sequential(*(list(model.children())[:-2]))
+
+        # orientation head, for orientation estimation
+        self.orientation = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, bins*2) # 4 bins
+        )
+
+        # confident head, for orientation estimation
+        self.confidence = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, bins) # 2 bins   
+        )
+
+        # dimension head
+        self.dimension = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(256, 3) # x, y, z
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 512)
+
+        orientation = self.orientation(x)
+        orientation = orientation.view(-1, self.bins, 2)
+        orientation = F.normalize(orientation, dim=2)
+        
+        confidence = self.confidence(x)
+
+        dimension = self.dimension(x)
+
+        return orientation, confidence, dimension
+
+if __name__ == '__main__':
+    # try resnet model 
+    resnet = resnet18(pretrained=True)
+    reg_model = ResNet18(model=resnet)
+
+    print(reg_model)
--- train.py
+++ train.py
@@ -1,16 +1,19 @@
 """
 Script for training Regressor Model
 """
+
 import argparse
 import os
 import sys
 from pathlib import Path
-import glob
 
 from script.Dataset import Dataset
+from script.Model import ResNet18, VGG11, OrientationLoss
 
 import torch
+import torch.nn as nn
 import torchvision
+from torchvision.models import resnet18, vgg11
 from torch.utils import data
 
 FILE = Path(__file__).resolve()
@@ -19,16 +22,34 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
+# model factory to choose model
+model_factory = {
+    'resnet18': resnet18(pretrained=True),
+    'vgg11': vgg11(pretrained=True)
+}
+regressor_factory = {
+    'resnet18': ResNet18,
+    'vgg11': VGG11
+}
+
 def train(
     epochs=10,
     batch_size=32,
     alpha=0.6,
     w=0.4,
     num_workers=4,
-    train_path=ROOT / 'dataset/KITTI/training'
+    lr=0.0001,
+    train_path=ROOT / 'dataset/KITTI/training',
+    model_path=ROOT / 'weights',
+    model_list='resnet18'
     ):
 
     # directory
+    train_path = str(train_path)
+    model_path = str(model_path)
+
+    # dataset
+    print('[INFO] Loading dataset...')
     dataset = Dataset(train_path)
 
     params = {
@@ -37,20 +58,47 @@ def train(
         'num_workers': num_workers
     }
 
-    # generator = data.DataLoader(dataset, **params)
+    # data generator
+    data_gen = data.DataLoader(dataset, **params)
+
+    # model
+    base_model = model_factory[model_list]
+    model = regressor_factory[model_list](model=base_model).cuda()
+    
+    # optimizer
+    opt_SGD = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    # loss function
+    conf_loss_func = nn.CrossEntropyLoss().cuda()
+    dim_loss_func = nn.MSELoss().cuda()
+    orient_loss_func = OrientationLoss
+
+    # load previous weights
+    latest_model = None
+    first_epoch = 0
+    if not os.path.isdir(model_path):
+        os.mkdir(model_path)
+    else:
+        try:
+            latest_model = [x for x in sorted(os.listdir(model_path)) if x.endswith('.pkl')][-1]
+        except:
+            pass
+
+    if latest_model is not None:
+        checkpoint = torch.load(model_path + latest_model)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        opt_SGD.load_state_dict(checkpoint['optimizer_state_dict'])
+        first_epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
+
+        print(f'[INFO] Using previous model {latest_model} at {first_epoch} epochs')
+        print('[INFO] Resuming training...')
+
+    total_num_batches = int(len(dataset)/batch_size)
 
 if __name__ == '__main__':
     train_path=str(ROOT / 'dataset/KITTI/training')
-    dataset = Dataset(train_path)
-    
-    for i in range(len(dataset)):
-        img = dataset[i][0]
-        label = dataset[i][1]
-
-        print(label)
-
-        if i > 3:
-            break
+    train()
 
 
 
