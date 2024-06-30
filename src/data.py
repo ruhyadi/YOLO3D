5c880d3d536718e28fea001d4c1760188fb9a47f
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-06-30 14:11:01 +0000

    feat: add data lightning module

--- src/data.py
+++ src/data.py
@@ -7,12 +7,13 @@ import rootutils
 ROOT = rootutils.autosetup()
 
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import numpy as np
 import torch
+from lightning import LightningDataModule
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import transforms
 from tqdm import tqdm
 
@@ -20,6 +21,83 @@ from src.schema import KittiDimsAvg, KittiSchema, MultibinDataSchema
 from src.utils import get_logger
 
 log = get_logger()
+
+
+class LitMultibinsData(LightningDataModule):
+    """Multibins data module."""
+
+    def __init__(
+        self,
+        images_dir: str = "data/kitti/image_2",
+        labels_dir: str = "data/kitti/label_2",
+        calib_path: str = "assets/kitti_calib.txt",
+        train_sets_path: str = "assets/kitti_train.txt",
+        val_sets_path: str = "assets/kitti_val.txt",
+        categories: List[str] = ["Car", "Pedestrian", "Cyclist"],
+        n_bins: int = 2,
+        overlap: float = 0.5,
+        batch_size: int = 32,
+        num_workers: int = 4,
+    ) -> None:
+        """Initialize KITTI datamodule."""
+        super().__init__()
+        self.images_dir = Path(images_dir)
+        self.labels_dir = Path(labels_dir)
+        self.calib_path = Path(calib_path)
+        self.train_sets_path = Path(train_sets_path)
+        self.val_sets_path = Path(val_sets_path)
+        self.categories = categories
+        self.n_bins = n_bins
+        self.overlap = overlap
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Setup the data for training and validation."""
+        if not self.data_train and not self.data_val:
+            self.data_train = MultibinsData(
+                images_dir=self.images_dir,
+                labels_dir=self.labels_dir,
+                calib_path=self.calib_path,
+                sets_path=self.train_sets_path,
+                categories=self.categories,
+                n_bins=self.n_bins,
+                bin_overlap=self.overlap,
+            )
+            self.data_val = MultibinsData(
+                images_dir=self.images_dir,
+                labels_dir=self.labels_dir,
+                calib_path=self.calib_path,
+                sets_path=self.val_sets_path,
+                categories=self.categories,
+                n_bins=self.n_bins,
+                bin_overlap=self.overlap,
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        """Load train dataloader."""
+
+        return DataLoader(
+            self.data_train,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        """Load validation dataloader."""
+
+        return DataLoader(
+            self.data_val,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+            shuffle=False,
+        )
 
 
 class MultibinsData(Dataset):
